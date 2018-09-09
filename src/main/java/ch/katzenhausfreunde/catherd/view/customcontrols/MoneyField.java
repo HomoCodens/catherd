17271fa3cd7ba6fdad0165991e385a066228581d
Severin Thöni <sethoeni@codeuntamed.ch>
2018-09-09 09:51:50 +0200

    Less hacky solution to MoneyField with listeners

--- src/main/java/ch/katzenhausfreunde/catherd/view/customcontrols/MoneyField.java
+++ src/main/java/ch/katzenhausfreunde/catherd/view/customcontrols/MoneyField.java
@@ -1,59 +1,51 @@
 package ch.katzenhausfreunde.catherd.view.customcontrols;
 
-import java.text.NumberFormat;
-import java.util.Locale;
-
-import javafx.beans.binding.Bindings;
 import javafx.beans.property.FloatProperty;
 import javafx.beans.property.SimpleFloatProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 import javafx.scene.control.TextField;
-import javafx.util.StringConverter;
 
 public class MoneyField extends TextField {
 	private FloatProperty amount;
-	private StringProperty nullSafeTextProperty;
 	
 	public MoneyField() {
 		super();
 		
+		this.amount = new SimpleFloatProperty();
+		
+		this.amountProperty().addListener((observable, oldValue, newValue) -> {
+			if(!this.isFocused()) {
+				setText(String.format("%.2f", newValue));
+			}
+		});
+		
+		this.textProperty().addListener((observable, oldValue, newValue) -> {			
+			if(oldValue == newValue) {
+				return;
+			}
+			
+			if(!newValue.matches("^\\d*(\\.\\d{0,2})?$")) {
+				setText(oldValue);
+				return;
+			}
+			
+			if(newValue == null || newValue.isEmpty()) {
+				setAmount(0.0f);
+			} else {
+				setAmount(Float.parseFloat(newValue));
+			}
+		});
+		
 		this.focusedProperty().addListener((observable, oldValue, newValue) -> {
 			if(newValue) {
-				System.out.println("We need to fuckus!");
 			} else {
 				String t = getText();
 				if(t == null || t.isEmpty()) {
 					setAmount(0.0f);
-				}
-				System.out.println("MoneyField out, over.");
-			}
-		});
-		
-		this.amount = new SimpleFloatProperty();
-		
-		this.nullSafeTextProperty = new SimpleStringProperty(null);
-		this.nullSafeTextProperty.bind(Bindings.when(textProperty().isNotNull().and(textProperty().isNotEqualTo(""))).then(textProperty()).otherwise("0.00"));
-		
-		this.textProperty().addListener((observable, oldValue, newValue) -> {
-			if(newValue == null) {
-				return;
-			} else {
-				if(!newValue.matches("^\\d*(\\.\\d{0,2})?$")) {
-					StringProperty textProperty = (StringProperty)observable;
-					textProperty.set(oldValue);
+				} else {
+					setText(String.format("%.2f", getAmount()));
 				}
 			}
 		});
-		
-		this.amountProperty().bind(Bindings.createFloatBinding(() -> {
-			String s = getText();
-			if(s == null || s.isEmpty()) {
-				return 0.0f;
-			} else {
-				return Float.parseFloat(s);
-			}
-		}, textProperty()));
 	}
 	
 	public float getAmount() {
@@ -61,17 +53,10 @@ public class MoneyField extends TextField {
 	}
 	
 	public void setAmount(float newAmount) {
-		//amount.set(newAmount);
-		
-		// Talk about hacks though...
-		this.textProperty().set(new Float(Math.floor(100*newAmount)/100).toString());
+		amount.set(newAmount);
 	}
 	
 	public FloatProperty amountProperty() {
 		return amount;
-	}
-	
-	public StringProperty nullSafeTextProperty() {
-		return nullSafeTextProperty;
 	}
 }
