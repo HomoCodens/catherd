304964ffd177bdac1da455ec6ea95270fe9a65f8
Severin Thöni <sethoeni@codeuntamed.ch>
2018-09-16 09:21:42 +0200

    Add contructor to VeterinaryMeasure

--- src/main/java/ch/katzenhausfreunde/catherd/model/VeterinaryMeasure.java
+++ src/main/java/ch/katzenhausfreunde/catherd/model/VeterinaryMeasure.java
@@ -2,8 +2,14 @@ package ch.katzenhausfreunde.catherd.model;
 
 import java.time.LocalDate;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
 import ch.katzenhausfreunde.catherd.util.CatHerdState;
+import ch.katzenhausfreunde.catherd.util.LocalDateAdapter;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class VeterinaryMeasure {
@@ -11,12 +17,20 @@ public class VeterinaryMeasure {
 	StringProperty cause;
 	StringProperty measures;
 	
+	public VeterinaryMeasure() {
+		date = new SimpleObjectProperty<LocalDate>(null);
+		cause = new SimpleStringProperty(null);
+		measures = new SimpleStringProperty(null);
+	}
+	
 	public void arm() {
 		date.addListener((observable, oldValue, newValue) -> CatHerdState.touchStore());
 		cause.addListener((observable, oldValue, newValue) -> CatHerdState.touchStore());
 		measures.addListener((observable, oldValue, newValue) -> CatHerdState.touchStore());
 	}
 	
+	@XmlJavaTypeAdapter(LocalDateAdapter.class)
+	@XmlElement(name = "date")
 	public final LocalDate getDate() {
 		return date.get();
 	}
