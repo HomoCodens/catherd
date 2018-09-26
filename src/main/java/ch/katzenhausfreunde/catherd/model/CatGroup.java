7420ed8b7e167110c90261f627dec828f6e418ae
Severin Thöni <sethoeni@codeuntamed.ch>
2018-09-26 18:58:51 +0200

    Fix adding cat not invalidating store

--- src/main/java/ch/katzenhausfreunde/catherd/model/CatGroup.java
+++ src/main/java/ch/katzenhausfreunde/catherd/model/CatGroup.java
@@ -3,6 +3,7 @@ package ch.katzenhausfreunde.catherd.model;
 import javax.xml.bind.annotation.XmlElement;
 
 import ch.katzenhausfreunde.catherd.util.CatHerdState;
+import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -37,7 +38,14 @@ public class CatGroup extends Nameable {
 	
 	public void arm() {
 		super.arm();
-		cats.addListener((Observable o) -> CatHerdState.touchStore());
+		cats.addListener(new InvalidationListener() {
+
+			@Override
+			public void invalidated(Observable arg0) {
+				CatHerdState.touchStore();
+			}
+			
+		});
 		cats.forEach((ct) -> ct.arm());
 	}
 }
